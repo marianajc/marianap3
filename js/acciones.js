c2d2bd5e1c090e99567097ec8b23ff9b966a286f
marianajc <coloradomendezf@gmail.com>
2014-06-02 09:01:12 -0500

    marianap3
    muestra 3 botones y al dar click muestran una imagen

---
+++ js/acciones.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+$(document).ready (function(){
+	$('#B1').click(function(){
+		$('#img1').show();
+	});//click
+ });//ready
+ $(document).ready (function(){
+	$('#B2').click(function(){
+		//$('#img1').hide();
+		$('#img2').show();
+	});//click
+ });//ready
+$(document).ready (function(){
+	$('#B3').click(function(){
+		//$('#img1').hide();
+		//$('#img2').hide();
+		$('#img3').show();
+	});//click
+ });//ready
